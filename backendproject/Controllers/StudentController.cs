b4d36fba0762c86a7a392f7871471767a9579f35
MutMutt <62054882+MutMutt@users.noreply.github.com>
2021-02-18 15:06:25 +0000

    Commit all the things

---
+++ backendproject/Controllers/StudentController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backendproject
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public static List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student() { Age = 18, Id = 24366,
+                First_Name = "Valentino", Last_Name = "Roux" });
+            students.Add(new Student()
+            {
+                Age = 20,
+                Id = 24356,
+                First_Name = "Gaetan",
+                Last_Name = "Jalef"
+            });
+            return students;
+        }
+
+        [HttpGet]
+
+        public IEnumerable<Student> GetStudents_List()
+        {
+            return GetStudents();
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<Student> Get_Student_byID(int id)
+        {
+            var student = GetStudents().Find(x => x.Id == id);
+            if(student != null)
+            {
+                return student;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ backendproject/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backendproject
+{
+    public class Student
+    {
+        public string First_Name { get; set; }
+
+        public string Last_Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int Id { get; set; }
+
+    }
+}
